fd9a6306d2f830112e4cf0d4ab17dea5b123d682
Rong-upenn <rongl@seas.upenn.edu>
2025-10-14 23:37:15 +0800

    Add HTTP URL

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -11,7 +11,7 @@ infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
 '''
 
 def connect_to_eth():
-	url = ""  # FILL THIS IN
+	url = "https://eth-mainnet.g.alchemy.com/v2/X_Baw5leGho7M_58y8Yma"  # FILL THIS IN
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 	return w3
