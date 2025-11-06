070494a2fe0ce15aed60aa138fe56890ac656853
Rong-upenn <rongl@seas.upenn.edu>
2025-11-06 13:31:45 +0800

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -24,48 +24,36 @@ contract Destination is AccessControl {
 
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
 		//YOUR CODE HERE
-		// Check if token is registered
-    	address wrappedToken = wrapped_tokens[_underlying_token];
-    	require(wrappedToken != address(0), "Token not registered");
-    
-    	// Mint wrapped tokens to recipient
-    	BridgeToken(wrappedToken).mint(_recipient, _amount);
-    
-    	// Emit event
-    	emit Wrap(_underlying_token, wrappedToken, _recipient, _amount);
-		
+		    address wrapped_token = wrapped_tokens[_underlying_token];
+        require(wrapped_token != address(0), "Wrapped token does not exist");
+
+        BridgeToken(wrapped_token).mint(_recipient, _amount);
+        emit Wrap(_underlying_token, wrapped_token, _recipient, _amount);	
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE
-		// Check if wrapped token is valid
-    	address underlyingToken = underlying_tokens[_wrapped_token];
-    	require(underlyingToken != address(0), "Invalid wrapped token");
-    
-    	// Burn wrapped tokens from sender
-    	BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
-    
-    	// Emit event
-    	emit Unwrap(underlyingToken, _wrapped_token, msg.sender, _recipient, _amount);
+	    address underlying_token = underlying_tokens[_wrapped_token];
+        require(underlying_token != address(0), "Underlying token does not exist");
+
+        BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
+        emit Unwrap(underlying_token, _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
 		//YOUR CODE HERE
-		// Check if token already exists
-    	require(wrapped_tokens[_underlying_token] == address(0), "Token already exists");
-    
-    	// Deploy new BridgeToken
-    	BridgeToken wrapped_token = new BridgeToken(_underlying_token, name, symbol, address(this));
-    	wrapped_tokens[_underlying_token] = address(wrapped_token);;
-		underlying_tokens[address(wrapped_token)] = _underlying_token;
-		tokens.push(_underlying_token);
-		
-		// Grant MINTER_ROLE to this contract
+	    require(wrapped_tokens[_underlying_token] == address(0), "Wrapped token already exists");
+
+        BridgeToken wrapped_token = new BridgeToken(_underlying_token, name, symbol, address(this));
+        wrapped_tokens[_underlying_token] = address(wrapped_token);
+        underlying_tokens[address(wrapped_token)] = _underlying_token;
+        tokens.push(_underlying_token);
+
+        // Grant MINTER_ROLE to this contract
         wrapped_token.grantRole(wrapped_token.MINTER_ROLE(), address(this));
 
-    	// Emit event
-    	emit Creation(_underlying_token, wrappedTokenAddress);
-    	return wrappedTokenAddress;
+        emit Creation(_underlying_token, address(wrapped_token));
+        return address(wrapped_token);
 
 	}
 
